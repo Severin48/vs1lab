0bced06413fe7c9a73ab103c3794155cc0f3fbe8
wado1012 <65955805+wado1012@users.noreply.github.com>
2020-12-09 17:19:56 +0100

    gta.js verbessert

--- Aufgabe3/gta_v3/gta-server.js
+++ Aufgabe3/gta_v3/gta-server.js
@@ -40,11 +40,24 @@ app.use(express.static(__dirname + "/public"));
  */
 
 // TODO: CODE ERGÄNZEN
-function GeoTag (latitudeTagging, longitudeTagging, name, hashtag ){
-    this.latitudeTagging = latitudeTagging;
-    this.lonitudeTaging = longitudeTagging;
+function GeoTag (latitude, longitude, name, hashtag ){
+    this.longitude = longitude;
+    this.latitude = latitude;
     this.name = name;
     this.hashtag = hashtag;
+
+    this.getLatitude = function(){
+        return this.latitude;
+    };
+    this.getLongitude = function(){
+        return this.longitude;
+    };
+    this.getName = function(){
+        return this.name;
+    };
+    this.getHashtag = function(){
+        return this.hashtag;
+    };
 }
 
 /**
@@ -57,33 +70,39 @@ function GeoTag (latitudeTagging, longitudeTagging, name, hashtag ){
  */
 
 // TODO: CODE ERGÄNZEN
-var taglist = [];
-function inMemory(){
-    let tag = new GeoTag(this.longitude, this.latitude, this.name, this.hashtag);
-    taglist.push(tag);
-}
-function searchName(){
-    var name = this.search;
-}
 
-
-/*
-    export function arraySpeicher (GeoTag){
-
+var InMemory = (function(){
+    var taglist = [];
+    return {
+        searchRadius: function (latitude, longitude, radius){
+            var resultList = taglist.filter(function (entry){
+                return (
+                    (Math.abs(entry.getLatitude()-latitude) < radius) &&
+                    (Math.abs(entry.getLongitude()-longitude) < radius)
+                );
+            });
+            return resultList;
+        },
+        searchBegriff: function (suchbegriff){
+            var resultList = taglist.filter(function (entry){
+                return (
+                    entry.getName().toString().includes(suchbegriff) || entry.getHashtag().toString().includes(suchbegriff)
+                );
+            });
+            return resultList;
+        },
+        add: function (GeoTag){
+            taglist.push;
+        },
+        delete: function (GeoTag){
+            taglist.splice(GeoTag.getCurrentPosition(),1);
+        }
     }
-    export function searchGeoTags(){
+})
 
-    }
-    export function searchSuchbegriff(){
 
-    }
-    export function addGeoTag(){
 
-    }
-    export function deleteGeoTags(){
 
-    }
-*/
 /**
  * Route mit Pfad '/' für HTTP 'GET' Requests.
  * (http://expressjs.com/de/4x/api.html#app.get.method)
@@ -94,8 +113,13 @@ function searchName(){
  */
 
 app.get('/', function(req, res) {
+    let lat = req.body.latitude;
+    let long = req.body.latitude;
     res.render('gta', {
-        taglist: []
+        taglist: InMemory.searchRadius(lat,long,5),
+        lat:lat,
+        long:long,
+        datatags: JSON.stringify(InMemory.searchRadius(lat,long,5))
     });
 });
 
@@ -113,13 +137,21 @@ app.get('/', function(req, res) {
  */
 
 // TODO: CODE ERGÄNZEN START
-app.get("/tagging", function (req, res){
-    res.send("Hello!!!")
-    req.body('tag-form');
-    res.render('gta', {
-        taglist: [GeoTag]
+app.post('tagging', function (req, res){
+    let lat = req.body.latitude;
+    let long = req.body.longitude;
+    let name = req.body.name;
+    let hashtag = req.body.hashtag;
+
+    InMemory.add(new GeoTag(lat,long,name,hashtag));
+
+    res.render('gta',{
+        taglist: InMemory.searchRadius(lat,long,5),
+        lat: lat,
+        long: long,
+        datatags: JSON.stringify(InMemory.searchRadius(lat,long,5))
     });
-})
+});
 
 /**
  * Route mit Pfad '/discovery' für HTTP 'POST' Requests.
@@ -134,12 +166,28 @@ app.get("/tagging", function (req, res){
  */
 
 // TODO: CODE ERGÄNZEN
-app.get('/discovery', function(req, res){
-    req.body('filter-form');
-    res.render('gta',{
-        taglist:[GeoTag]
-    });
-})
+app.post('/discovery', function(req, res){
+    var lat = req.body.hid_lat;
+    var long = req.body.hid_long;
+    var term = req.body.search;
+
+    if (term){
+        res.render('gta',{
+            taglist: InMemory.searchBegriff(term),
+            lat: lat,
+            long: long,
+            datatags: JSON.stringify(InMemory.searchBegriff(term))
+        })
+    } else {
+        res.render('gta',{
+            taglist: InMemory.searchRadius(lat,long,5),
+            lat: lat,
+            long: long,
+            datatags: JSON.stringify(InMemory.searchRadius(lat,long,5))
+        })
+    }
+
+});
 /**
  * Setze Port und speichere in Express.
  */
@@ -158,3 +206,4 @@ var server = http.createServer(app);
  */
 
 server.listen(port);
+
