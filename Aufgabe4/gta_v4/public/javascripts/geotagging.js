213eedbbb6cde4e72eefda40ec001819918adb8d
Severin48 <severin2448@gmail.com>
2021-01-23 15:39:20 +0100

    Fixed comment

--- Aufgabe4/gta_v4/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4/public/javascripts/geotagging.js
@@ -169,7 +169,7 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
 
 submitTag.addEventListener("click", function() {
     console.log("Adding")
-    ajax.open("POST", "/geotags", true); /TODO: changed url: geotags
+    ajax.open("POST", "/geotags", true);
     ajax.setRequestHeader("Content-Type", "application/json");
     ajax.responseType = "json";
 
